a2db9d96badafb33f357f551c49152c419148326
mastar3104 <hcs.sasajima@gmail.com>
2024-03-29 13:13:37 +0900

    bug: Club Entityを加減算して保存すると同時リクエストで差異が発生する可能背があるため、Queryで加減算を行うよう修正

--- src/main/kotlin/com/example/demo/domain/entity/Employee.kt
+++ src/main/kotlin/com/example/demo/domain/entity/Employee.kt
@@ -1,5 +1,6 @@
 package com.example.demo.domain.entity
 
+import com.example.demo.domain.event.EmployeeChangeClubEvent
 import com.example.demo.domain.value.ClubId
 import com.example.demo.domain.value.EmployeeId
 import com.example.demo.domain.value.TeamId
@@ -34,28 +35,28 @@ class Employee private constructor(
             name: String,
             teamId: TeamId?,
             clubId: ClubId?,
-        ): Employee {
+        ): Pair<Employee, EmployeeChangeClubEvent.EmployeeCreate> {
             return Employee(
                 id = EmployeeId.create(),
                 name = name,
                 teamId = teamId ?: TeamId.DEFAULT,
                 clubId = clubId ?: ClubId.NOT_BELONG,
                 clubApproval = false,
-            )
+            ) to EmployeeChangeClubEvent.EmployeeCreate
         }
     }
 
-    fun deleteClub(): Employee {
+    fun deleteClub(): Pair<Employee, EmployeeChangeClubEvent> {
         return Employee(
             id = id,
             name = name,
             teamId = teamId,
             clubId = ClubId.NOT_BELONG,
             clubApproval = false,
-        )
+        ) to EmployeeChangeClubEvent.Change(oldClubId = clubId)
     }
 
-    fun joinClub(newClubId: ClubId): Employee {
+    fun joinClub(newClubId: ClubId): Pair<Employee, EmployeeChangeClubEvent> {
         if (clubId.isNotBelongId) {
             if (newClubId.isNotBelongId) {
                 throw IllegalArgumentException("未所属クラブに新規参加することはできません。")
@@ -72,7 +73,7 @@ class Employee private constructor(
             teamId = teamId,
             clubId = newClubId,
             clubApproval = false,
-        )
+        ) to EmployeeChangeClubEvent.Change(oldClubId = clubId)
     }
 }
 
--- src/main/kotlin/com/example/demo/infrastructure/repository/EmployeeRepositoryImpl.kt
+++ src/main/kotlin/com/example/demo/infrastructure/repository/EmployeeRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.demo.infrastructure.repository
 
 import com.example.demo.domain.entity.Employee
+import com.example.demo.domain.event.EmployeeChangeClubEvent
 import com.example.demo.domain.repository.EmployeeRepository
 import com.example.demo.domain.value.ClubId
 import com.example.demo.domain.value.EmployeeId
@@ -27,7 +28,8 @@ class EmployeeRepositoryImpl(
         }
     }
 
-    override fun save(employee: Employee) {
+    // トランザクショナル
+    override fun save(employee: Employee, employeeChangeClubEvent: EmployeeChangeClubEvent) {
         val resource = mapOf(
             "id" to employee.id.toString(),
             "name" to employee.name,
@@ -36,5 +38,17 @@ class EmployeeRepositoryImpl(
             "clubApproval" to if (employee.clubApproval) "true" else "false",
         )
         database.employeesAdd(resource)
+        when(employeeChangeClubEvent) {
+            is EmployeeChangeClubEvent.Change -> {
+                // UPDATE SET numberOfEmployee + 1 WHERE id = :updateOldClubId
+                database.clubAddNumberOfEmployee(employee.clubId.toString())
+                // UPDATE SET numberOfEmployee - 1 WHERE id = :updateNewClubId
+                database.clubSubNumberOfEmployee(employeeChangeClubEvent.oldClubId.toString())
+            }
+            is EmployeeChangeClubEvent.EmployeeCreate -> {
+                // UPDATE SET numberOfEmployee + 1 WHERE id = :updateOldClubId
+                database.clubAddNumberOfEmployee(employee.clubId.toString())
+            }
+        }
     }
 }
